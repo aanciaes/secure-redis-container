f44edc5b4602a90f9bbea7aafc167fd4af96d765
aanciaes <aanciaes.miguel@gmail.com>
2020-09-24 01:47:16 +0100

    Added some exception handling

--- attestation_server/server.cpp
+++ attestation_server/server.cpp
@@ -18,20 +18,20 @@ std::string signData(std::string data) {
   CkPfxW pfx;
   bool success = pfx.LoadPfxFile(L"attst-server.p12",L"qwerty");
   if (success != true) {
-    wprintf(L"%s\n",pfx.lastErrorText());
+    throw 5477;
   }
 
   // Get the default private key.
   CkPrivateKeyW *privKey = pfx.GetPrivateKey(0);
   if (pfx.get_LastMethodSuccess() != true) {
-    wprintf(L"%s\n",pfx.lastErrorText());
+    throw 6455;
   }
 
   // Import the private key into the RSA component:
   success = rsa.ImportPrivateKeyObj(*privKey);
   if (success != true) {
-    wprintf(L"%s\n",rsa.lastErrorText());
     delete privKey;
+    throw 6456;
   }
 
   delete privKey;
@@ -59,6 +59,36 @@ std::string signData(std::string data) {
   return base64SigStr;
 }
 
+std::string hashFile (std::string path) {
+	const char * filePath = path.c_str();
+
+	CkCrypt2 crypt;
+
+    // Choose the hash algorithm.
+    // Can be  "sha1", "sha256", "sha384", "sha512", "md2", "md5", "haval", "ripemd128", "ripemd160","ripemd256", or "ripemd320".
+    crypt.put_HashAlgorithm("sha256");
+
+    CkByteData hashBytes;
+    bool success;
+    success = crypt.HashFile(filePath, hashBytes);
+
+    if (!success) {
+    	throw 43552;
+    }
+    
+    CkStringBuilder sb;
+    sb.AppendEncoded(hashBytes,"hex");
+
+    std::string hash = sb.getAsString();
+    
+    // convert string to lower case
+    std::for_each(hash.begin(), hash.end(), [](char & c) {
+        c = ::tolower(c);
+    });
+
+    return hash;
+}
+
 int main(void) {
 
   using namespace httplib;
@@ -67,38 +97,54 @@ int main(void) {
 
   svr.Get("/attest", [](const httplib::Request &, httplib::Response &res) {
   
-    CkCrypt2 crypt;
+  	try {
+  		std::string hash = hashFile("/home/attestation_server/file.txt");
+    	std::string signedData = signData(hash);
 
-    // Choose the hash algorithm.
-    // Can be  "sha1", "sha256", "sha384", "sha512", "md2", "md5", "haval", "ripemd128", "ripemd160","ripemd256", or "ripemd320".
-    crypt.put_HashAlgorithm("sha256");
+    	std::string response;
+   		response.append("{\r\n");
+    	response.append("\t\"filename\": \"/home/attestation_server/file.txt\",\r\n");
+    	response.append("\t\"sha256\": \"");
+    	response.append(hash);
+    	response.append("\",\r\n\t\"signature\": \"");
+    	response.append(signedData);
+    	response.append("\"\r\n}");
 
-    CkByteData hashBytes;
-    bool success;
-    success = crypt.HashFile("/home/attestation_server/file.txt",hashBytes);
+    	std::cout << "--- Started Attestation Procedure ---" << std::endl;
+    	std::cout << "\t file.txt: " << hash << std::endl;
+    	std::cout << "\t sig: " << signedData << std::endl;
+    	std::cout << "--- End of Attestation Procedure ---" << std::endl;
 
-    
-    CkStringBuilder sb;
-    sb.AppendEncoded(hashBytes,"hex");
+    	res.set_content(response, "application/json");
+  	} catch (int errorCode) {
 
-    std::string hash = sb.getAsString();
-    std::string signedData = signData(hash);
+  		std::string response;
+   		response.append("{\r\n");
 
-    std::string response;
-    response.append("{\r\n");
-    response.append("\t\"filename\": \"/home/attestation_server/file.txt\",\r\n");
-    response.append("\t\"sha256\": \"");
-    response.append(hash);
-    response.append("\",\r\n\t\"signature\": \"");
-    response.append(signedData);
-    response.append("\"\r\n}");
+   		switch (errorCode) {
+   			case 43552:
+    			response.append("\t\"error\": \"An unexpected error occured while hashing the file\",\r\n");
+    			break;
+    		case 5477:
+    			response.append("\t\"error\": \"An unexpected error occured while loading attestation keystore\",\r\n");
+    			break;
+    		case 6455:
+    			response.append("\t\"error\": \"An unexpected error occured while loading attestation private key\",\r\n");
+    			break;
+    		case 6456:
+    			response.append("\t\"error\": \"An unexpected error occured while loading attestation private key\",\r\n");
+    			break;
+    		default:
+    			response.append("\t\"error\": \"An unexpected error occured.\",\r\n");
+   		}
+   		
+   		response.append("\r\n\t\"errorCode\": \"");
+    	response.append(std::to_string(errorCode));
+    	response.append("\"\r\n}");
 
-    std::cout << "--- Started Attestation Procedure ---" << std::endl;
-    std::cout << "\t file.txt: " << hash << std::endl;
-    std::cout << "\t sig: " << signedData << std::endl;
-    std::cout << "--- End of Attestation Procedure ---" << std::endl;
-
-    res.set_content(response, "application/json");
+  		res.set_content(response, "application/json");
+  		res.status = 500;
+  	}
   });
 
   std::cout << "Server started at 0.0.0.0 port 8080" << std::endl;
