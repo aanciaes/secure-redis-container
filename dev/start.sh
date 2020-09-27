6d6bc56759eb528e571bbe0f2de29489410ecc85
aanciaes <aanciaes.miguel@gmail.com>
2020-09-27 15:19:57 +0100

    Attestation of attestation server for dev. #16

--- dev/start.sh
+++ dev/start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-/home/attestation_server/server &
-SCONE_HASH=1 redis-server > /home/attestation_server/mrenclave
+SCONE_HASH=1 /home/attestation_server/server > /home/attestation_server/attst_server_mrenclave
+SCONE_VERSION=1 /home/attestation_server/server &
+SCONE_HASH=1 redis-server > /home/attestation_server/redis_mrenclave
 #chmod 444 /home/attestation_server/mrenclave
 SCONE_VERSION=1 redis-server
